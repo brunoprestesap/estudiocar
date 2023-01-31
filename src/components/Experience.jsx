c330d26bf283468f2179ecd07691aaa976e0d328
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-30 23:44:54 -0300

    secão experience completa

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,13 +6,13 @@ const Experience = () => {
     <div className="relative">
       <img
         src={bgExperience}
-        className="w-full h-96 md:h-screen object-cover"
+        className="w-full h-96  object-cover"
         alt="background experiencia"
       />
 
       <div className="bg-white/50 w-full h-full absolute top-0 left-0">
-        <div className="h-full flex justify-around items-center">
-          <div className="w-80 md:w-96 m-5 md:m-0 group bg-stone-800 bg-opacity-90 drop-shadow-lg bg-blend-screen text-stone-200 py-8 px-5">
+        <div className="h-full w-full flex justify-around items-center p-5 gap-3">
+          <div className="w-80 md:w-96 md:m-0 group bg-stone-800 bg-opacity-90 drop-shadow-lg bg-blend-screen text-stone-200 py-8 px-5">
             <h2 className="font-bold text-xl md:text-3xl">Experiência</h2>
 
             <div>
@@ -28,17 +28,17 @@ const Experience = () => {
               <div className="flex justify-between items-center mt-4 border-l-4 pl-2 border-l-gray-200">
                 <h2 className="text-5xl lg:text-5xl">1K+</h2>
                 <h2 className="text-sm lg:text-xl text-right">
-                  Pacientes
+                  Serviços
                   <br />
-                  atendidos
+                  Realizados
                 </h2>
               </div>
             </div>
           </div>
 
-          <div className="md:w-96">
-            <h2 className="text-3xl md:text-6xl text-center md:text-justify font-bold text-gray-900">
-              Baseado em Experiência
+          <div>
+            <h2 className="align-middle	text-2xl leading-7 tracking-wider md:text-6xl text-right md:text-justify font-bold text-gray-900">
+              Baseado em<br/> Experiência
             </h2>
           </div>
         </div>
